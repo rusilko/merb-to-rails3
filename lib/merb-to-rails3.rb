bf10e12fea05505a54e25db3e4e59ad192a21cd9
Marcin Kulik <marcin.kulik@llp.pl>
2010-04-02 12:22:42 +0200

    Removed deprecated railtie_name call

--- lib/merb-to-rails3.rb
+++ lib/merb-to-rails3.rb
@@ -5,8 +5,6 @@ require "#{dir}/controller_exceptions"
 
 module MerbToRails3
   class Railtie < Rails::Railtie
-    railtie_name :"merb-to-rails3"
-
     initializer "merb-to-rails3.rails" do
       class ::ActionController::Base
         extend  MerbToRails3::Helpers::Controller
